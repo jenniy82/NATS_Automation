a4101cb9cd7f66be731605f9829858212950ddc1
Yevgeny Arman <yevgeny.arman@forescout.com>
2020-04-02 13:47:46 +0300

    Nats project

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ attributes_for_hash = ["sIP","cIP"]
 
 
 # input that will be sent to NATS
-input_to_Nats = '{"properties":{"sourceIp":"10.160.178.30","sourceNodeID":"2687540205173066847","sourceName":"FLOW"},"sessions":[{"prot":6,"sIP":"10.160.178.36","cIP":"10.160.112.1","sPrt":22,"cPrt":62198,"time":0,"bid":true,"exporterIp":"127.0.0.1","dirConf":"HIGH"},{"prot":17,"sIP":"10.160.178.20","cIP":"10.160.112.1","sPrt":31364,"cPrt":62397,"time":1584292383,"bid":false,"exporterIp":"127.0.0.1","dirConf":"LOW"}]}'
-
+input_to_Nats = '{"properties":{"sourceIp":"10.160.178.30","sourceNodeID":"2687540205173066847","sourceName":"FLOW"},"sessions":[{"prot":5,"sIP":"10.160.178.36","cIP":"10.160.112.1","sPrt":22,"cPrt":62198,"time":0,"bid":true,"exporterIp":"127.0.0.1","dirConf":"HIGH"},{"prot":17,"sIP":"10.160.178.20","cIP":"10.160.112.1","sPrt":31364,"cPrt":62397,"time":1584292383,"bid":false,"exporterIp":"127.0.0.1","dirConf":"LOW"}]}'
+print("start")
 # we need internel ip for grepping the log file
 internal_ip_in_flow = '10.160.178.36'
 
@@ -24,20 +24,22 @@ print( "session list from input: ", sessions_list)
 output_handler = Output_handler('10.160.178.30','root','aristo1', (f"""'{input_to_Nats}'"""))
 
 # publish the input to NATS by copying and running the script on appliance -> look at the output in the log -> convert the output in the log to json format with regex
-output_string = output_handler.get_output(internal_ip_in_flow)
+# output_string = output_handler.get_output(internal_ip_in_flow)
 
 # get the payload list(flows) from the json output
-output_payload_list = output_handler.get_payload_list(output_string)
-print("output payload list: ",output_payload_list)
+# output_payload_list = output_handler.get_payload_list(output_string)
+# print("output payload list: ",output_payload_list)
+
+pulsar_output_payload_list = output_handler.get_pulsar_output('pulsar://10.160.178.100:6650','internal/flowsUploader/flows')
 
 # compare input output class
-compare_input_output = Compare_input_output(sessions_list,output_payload_list,attributes,attributes_for_hash)
+compare_input_output = Compare_input_output(sessions_list,pulsar_output_payload_list,attributes,attributes_for_hash)
 
 # The flows in input and output are not in the same order, for comparison we need to find the relevant flow by identifier, in this case the flow identifier will be hash of number of flow values the keys of these values can be configurable.
 input_hash_map = compare_input_output.create_hash_map_for_flows(sessions_list)
 print ("input hash map: ",input_hash_map)
 
-output_hash_map = compare_input_output.create_hash_map_for_flows(output_payload_list)
+output_hash_map = compare_input_output.create_hash_map_for_flows(pulsar_output_payload_list)
 print ("output hash map: ",output_hash_map)
 
 compare_input_output.compare_input_output(input_hash_map,output_hash_map)
--- output_handler.py
+++ output_handler.py
@@ -2,7 +2,7 @@ from counterAct_actions import *
 import time
 import json
 import re
-
+from pulsar_consumer import *
 
 attributes = ['sIP','cIP']
 
@@ -17,17 +17,36 @@ class Output_handler:
 
     def get_output(self, internal_ip):
         counterAct = CounterACT_actions(self.CounterACT,self.CounterACT_User,self.CounterACT_Password)
-        counterAct.copy_file_to_appliance('\publisher_test.py','/tmp')
+        counterAct.copy_file_to_appliance('/publisher_test.py','/tmp')
         counterAct.run_command(f"""tail -f /usr/local/forescout/log/plugin/flowsuploader/flowsuploader.log | stdbuf -o0 grep {internal_ip} | stdbuf -o0 grep 'PulsarUploader.publish' > /usr/local/forescout/log/plugin/flowsuploader/automation &""")
-        counterAct.run_command(f"""/opt/rh/rh-python36/root/usr/bin/python -u /tmp/pycharm_project_348/publisher_test.py {self.flow_input}""")
-        time.sleep( 10 )
+        counterAct.run_command(f"""/opt/rh/rh-python36/root/usr/bin/python -u /tmp/publisher_test.py {self.flow_input}""")
+        time.sleep( 15 )
         output = counterAct.run_command("cat /usr/local/forescout/log/plugin/flowsuploader/automation")
         print(output)
         output = str(output).replace('protocol','prot')
         output_string = re.findall(r'{".*}',output)
+        print("output string: ")
+        print(output_string)
         counterAct.run_command("""kill -9 `pidof tail`""")
-        counterAct.run_command("rm -rf /usr/local/forescout/log/plugin/flowsuploader/automation")
+        # counterAct.run_command("rm -rf /usr/local/forescout/log/plugin/flowsuploader/automation")
         return output_string
+
+
+    def get_pulsar_output(self, pulsarIP, pulsarTopic):
+        counterAct = CounterACT_actions(self.CounterACT,self.CounterACT_User,self.CounterACT_Password)
+        counterAct.copy_file_to_appliance('/publisher_test.py','/tmp')
+        pulsar_consumer = Pulsar_consumer(pulsarIP,pulsarTopic)
+        consumer = pulsar_consumer.start_consumer()
+        counterAct.run_command(f"""/opt/rh/rh-python36/root/usr/bin/python -u /tmp/publisher_test.py {self.flow_input}""")
+        pulsar_output = pulsar_consumer.get_pulsar_output(consumer)
+        print(pulsar_output)
+        output_string = pulsar_output.decode("utf-8")
+        output_string = output_string.replace("protocol","prot")
+        output_json = json.loads(output_string)
+        payload = output_json["payload"]
+        return payload
+
+
 
 
 
